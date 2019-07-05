33c80ba06417c8f26687fbb207932739ef2c31e1
Sunset1234 <emmanuel_burciaga@hotmail.com>
2019-07-04 21:22:07 -0500

    Subiendo los primeros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
